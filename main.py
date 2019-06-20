566d33cef8dbac97e3e34c0090c7c8c72226f6cd
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-20 22:59:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum in optimizer')
 parser.add_argument('-bs', '--batchsize', type=int, default=1, help='batch size')
-parser.add_argument('--epochs', type=int, default=300, help='epochs to train')
+parser.add_argument('--epochs', type=int, default=500, help='epochs to train')
 parser.add_argument('-out', '--outf', type=str, default='./model_checkpoint', help='path to save model checkpoints')
 config = parser.parse_args()
 num_classes = 4
@@ -164,7 +164,7 @@ for epoch in range(config.epochs):
         print(('epoch %d | mean test acc: %f') % (epoch+1, np.mean(test_acc_epoch)))
         torch.save(classifier.state_dict(), '%s/%s_model_%d.pth' % (config.outf, 'fudanc0', epoch))
     else:
-        if epoch<100:
+        if epoch<150:
             for i, data in enumerate(traindataloader0001):
                 slices,label = data
                 slices, label = slices.to(device), label.to(device)
@@ -215,7 +215,7 @@ for epoch in range(config.epochs):
             print(('epoch %d | mean test acc: %f') % (epoch+1, np.mean(test_acc_epoch)))
             torch.save(classifier.state_dict(), '%s/%s_model_%d.pth' % (config.outf, 'fudanc0', epoch)) 
         else:
-            if epoch<150:
+            if epoch<250:
                 for i, data in enumerate(traindataloader001):
                     slices,label = data
                     slices, label = slices.to(device), label.to(device)
