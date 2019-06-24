598c7e2eb0efbc9437ea4926f752fe02afb38ee1
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-24 08:20:34 +0100

    Update fudandataset.py

--- fudandataset.py
+++ fudandataset.py
@@ -38,9 +38,9 @@ def my_segmentation_transform(input1, target1):
                 input2, target = F.affine(input2, *affine_params), F.affine(target, *affine_params)
             
             input2 = np.array(input2)            
-            input2= input2.astype("uint16")        
+            input2= input2.astype("int16")        
             target= np.array(target)
-            target= target.astype("uint16")
+            target= target.astype("int16")
             inout.append(input2[:,:,np.newaxis].transpose(2,0,1))
             tarout.append(target)
         return inout, tarout 
