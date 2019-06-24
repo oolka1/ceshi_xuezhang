1bae43cf9f4c8f3af7a2f7c7526136fdfd705e07
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-24 05:36:16 +0100

    Update fudandataset.py

--- fudandataset.py
+++ fudandataset.py
@@ -35,7 +35,7 @@ class fudandataset(data.Dataset):
                         labels[labels==200]=1
                         labels[labels==500]=2
                         labels[labels==600]=3
-                        self.train_labels.append(labels)
+                        self.train_labels.append(labels[50:200, 50:200])
                 else:
                     file_path = os.path.join(self.root,file_name)
                     file_data = nib.load(file_path)
@@ -43,6 +43,7 @@ class fudandataset(data.Dataset):
                     d = file_data.shape[2]
                     for i in range(d):
                         data = copy.deepcopy(file_data[:,:,i])
+                        data = data[50:200, 50:200]
                         self.train_data.append(data[:,:,np.newaxis].transpose(2,0,1))
         else:
             print('loading test data ')
@@ -61,7 +62,7 @@ class fudandataset(data.Dataset):
                         labels[labels==200]=1
                         labels[labels==500]=2
                         labels[labels==600]=3
-                        self.test_labels.append(labels)
+                        self.test_labels.append(labels[50:200, 50:200])
                 else:
                     file_path = os.path.join(self.root,file_name)
                     file_data = nib.load(file_path)
@@ -69,6 +70,7 @@ class fudandataset(data.Dataset):
                     d = file_data.shape[2]
                     for i in range(d):
                         data = copy.deepcopy(file_data[:,:,i])
+                        data = data[50:200, 50:200]
                         self.test_data.append(data[:,:,np.newaxis].transpose(2,0,1))
                         
     def __getitem__(self, index):
