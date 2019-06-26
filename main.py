b861c66f3fad849da731726030ed3d639c940d1b
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-26 07:51:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from fudandataset import fudandataset
 from Unet import UNet_Nested
 import copy
 
-traindata_root = "train"
-testdata_root = "test"
+traindata_root = "train1"
+testdata_root = "test1"
 log_root = "log"
 if not os.path.exists(log_root): os.mkdir(log_root)
 LOG_FOUT = open(os.path.join(log_root, 'train.log'), 'w')
