0aa42f3726db7ca5a170b36884539a9ded2268d2
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-28 03:17:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ for epoch in range(config.epochs):
     loss_stroge = np.mean(test_loss_epoch)
     torch.save(classifier.state_dict(), '%s/%s_model_%d.pth' % (config.outf, 'fudanc0', epoch))
     if loss_stroge > previous_loss:          
-        lr = lr * 0.9
+        lr = lr * 0.1
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr               
     previous_loss = loss_stroge
