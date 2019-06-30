4e87c485489f19442e8f7072f0a48f031b4d9255
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-30 08:26:01 +0100

    Update fudandataset.py

--- fudandataset.py
+++ fudandataset.py
@@ -81,8 +81,8 @@ class fudandataset(data.Dataset):
                         if random.random()>0.5:
                             positionx = random.randint(0, 1)
                             positiony = random.randint(0, 1)
-                            image = F.affine(image, angle=0,translate=[positionx,positiony],shear=0)
-                            segmentation = F.affine(segmentation, angle=0,translate=[positionx,positiony],shear=0)
+                            image = F.affine(image, angle=0,translate=[positionx,positiony],scale=[1,1],shear=0)
+                            segmentation = F.affine(segmentation, angle=0,translate=[positionx,positiony],scale=[1,1],shear=0)
                         if random.random()>0.5:
                             image = F.hflip(image)
                             segmentation = F.hflip(segmentation)
