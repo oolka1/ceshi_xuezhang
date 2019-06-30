cd4af87c8f8631f45a82ef0c55d5144f1a3b794f
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-30 20:49:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def log_string(out_str):
 os.system('mkdir {0}'.format('model_checkpoint'))
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', type=float, default=0.0005, help='learning rate')
+parser.add_argument('--lr', type=float, default=0.005, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum in optimizer')
 parser.add_argument('-bs', '--batchsize', type=int, default=1, help='batch size')
 parser.add_argument('--epochs', type=int, default=100, help='epochs to train')
@@ -50,7 +50,7 @@ classifier = UNet_Nested(n_classes = num_classes)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 classifier.to(device)
 lr=config.lr
-optimizer = optim.SGD(classifier.parameters(), lr=lr,weight_decay = 1e-2)
+optimizer = optim.SGD(classifier.parameters(), lr=lr,weight_decay = 2e-3)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 
 #loss = nn.CrossEntropyLoss()
