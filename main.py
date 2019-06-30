a78a9f5c99481c535600b447a837a728b14be0ea
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-06-30 01:21:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def log_string(out_str):
 os.system('mkdir {0}'.format('model_checkpoint'))
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', type=float, default=0.00001, help='learning rate')
+parser.add_argument('--lr', type=float, default=0.00005, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum in optimizer')
-parser.add_argument('-bs', '--batchsize', type=int, default=5, help='batch size')
+parser.add_argument('-bs', '--batchsize', type=int, default=3, help='batch size')
 parser.add_argument('--epochs', type=int, default=200, help='epochs to train')
 parser.add_argument('-out', '--outf', type=str, default='./model_checkpoint', help='path to save model checkpoints')
 config = parser.parse_args()
@@ -55,7 +55,7 @@ classifier = UNet_Nested(n_classes = num_classes)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 classifier.to(device)
 lr=config.lr
-optimizer = optim.Adam(classifier.parameters(), lr=lr,weight_decay = 1e-2)
+optimizer = optim.Adam(classifier.parameters(), lr=lr,weight_decay = 1e-3)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 
 #loss = nn.CrossEntropyLoss()
