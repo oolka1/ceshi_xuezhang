d25b3dcda276535cba02ec161772c379b6bf923c
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-01 06:43:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=0.00005, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum in optimizer')
 parser.add_argument('-bs', '--batchsize', type=int, default=1, help='batch size')
-parser.add_argument('--epochs', type=int, default=100, help='epochs to train')
+parser.add_argument('--epochs', type=int, default=40, help='epochs to train')
 parser.add_argument('-out', '--outf', type=str, default='./model_checkpoint', help='path to save model checkpoints')
 config = parser.parse_args()
 num_classes = 4
