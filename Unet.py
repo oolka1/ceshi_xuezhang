765bf7aa7421768c77ddacedf17ccdfcf3e73eaf
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-03 05:22:06 +0100

    Update Unet.py

--- Unet.py
+++ Unet.py
@@ -9,7 +9,7 @@ from utils import init_weights, count_param
 class UNet_Nested(nn.Module):
     
 
-    def __init__(self, in_channels=1, n_classes=2, feature_scale=2, is_deconv=True, is_batchnorm=True):
+    def __init__(self, in_channels=1, n_classes=2, feature_scale=1, is_deconv=True, is_batchnorm=True):
         super(UNet_Nested, self).__init__()
         self.in_channels = in_channels
         self.feature_scale = feature_scale
