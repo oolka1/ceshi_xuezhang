74b604a68e4676da9f2e48f54c0eb89732107d47
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-04 07:38:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,8 @@ classifier.to(device)
 optimizer = optim.Adam(classifier.parameters(), lr=config.lr)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 train_acc_epoch, test_acc_epoch ,train_loss_epoch,test_loss_epoch= [], [],[],[]
-#loss = nn.CrossEntropyLoss()
+weight_1 = torch.Tensor([1,4])
+output = nn.CrossEntropyLoss(weight=weight_1)
 
 print ('Starting training...\n')
 for epoch in range(config.epochs):
@@ -68,7 +69,9 @@ for epoch in range(config.epochs):
         pred = classifier(slices)
         pred = pred.view(-1, num_classes)
         label = label.view(-1).long()
-        loss = F.cross_entropy(pred, label)
+        #loss = F.cross_entropy(pred, label)
+        loss = output(pred, label)
+                
         #print(pred.size(),label.size())
         loss.backward()
         optimizer.step()
@@ -92,7 +95,9 @@ for epoch in range(config.epochs):
                 pred = classifier(slices)
                 pred = pred.view(-1, num_classes)
                 label = label.view(-1).long()
-                loss = F.cross_entropy(pred, label)
+                #loss = F.cross_entropy(pred, label)
+                loss = output(pred, label)
+                
                 pred_choice = pred.data.max(1)[1]
                 correct = pred_choice.eq(label.data).cpu().sum()
                 test_acc = correct.item()/float(label.shape[0])
