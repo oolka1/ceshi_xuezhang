19a19f47cec944b17a32b2156a57f54decf6b790
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-08 20:00:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ os.system('mkdir {0}'.format('model_checkpoint'))
 parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum in optimizer')
-parser.add_argument('-bs', '--batchsize', type=int, default=32, help='batch size')
+parser.add_argument('-bs', '--batchsize', type=int, default=1, help='batch size')
 parser.add_argument('--epochs', type=int, default=400, help='epochs to train')
 parser.add_argument('-out', '--outf', type=str, default='./model_checkpoint', help='path to save model checkpoints')
 config = parser.parse_args()
@@ -40,7 +40,7 @@ num_classes = 2
 train_dataset = fudandataset(traindata_root,train=True)
 test_dataset = fudandataset(testdata_root,train=False)
 
-traindataloader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batchsize, shuffle=True, 
+traindataloader = torch.utils.data.DataLoader(train_dataset, batch_size=32*(config.batchsize), shuffle=True, 
                                               num_workers=4)
 testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=config.batchsize, shuffle=True, 
                                               num_workers=4)
