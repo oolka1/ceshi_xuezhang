97c4dc73716c47470d4ff052018fa621e9e811c7
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-13 20:54:14 +0100

    Update fudandataset.py

--- fudandataset.py
+++ fudandataset.py
@@ -66,7 +66,7 @@ class fudandataset(data.Dataset):
                         max1=data.max()
                         max1=max1.astype(np.float32)
                         data=data/max1  
-                        self.train_data1.append(data.transpose(2,0,1))
+                        self.train_data1.append(data)
             for j in range(30):
                 if j<29:
                     for i in range(len(self.train_data1)):
