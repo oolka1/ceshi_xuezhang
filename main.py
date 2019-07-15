a790a56cfac44381de3f4db08349d0e3105bfd8f
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-15 12:01:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ parser.add_argument('-bs', '--batchsize', type=int, default=1, help='batch size'
 parser.add_argument('--epochs', type=int, default=600, help='epochs to train')
 parser.add_argument('-out', '--outf', type=str, default='./model_checkpoint', help='path to save model checkpoints')
 config = parser.parse_args()
-num_classes = 2
+num_classes = 4
 train_dataset = fudandataset(traindata_root,train=True)
 test_dataset = fudandataset(testdata_root,train=False)
 
