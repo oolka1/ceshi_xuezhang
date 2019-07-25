5b0563ce9926ee455bc4a7906c8d11c3c609a01c
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-25 05:21:18 +0100

    Update loss.py

--- loss.py
+++ loss.py
@@ -3,16 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 class DiceLoss(nn.Module):
-    def __init__(self,smooth=1):
+    def __init__(self, class_num=2,smooth=1):
         super(DiceLoss, self).__init__()
         self.smooth = smooth
+        self.class_num = class_num
 
-
-    def forward(self,input, target, class_num):
+    def forward(self,input, target):
         input = torch.exp(input)
         self.smooth = 0.
         Dice = Variable(torch.Tensor([0]).float()).cuda()
-        for i in range(1,class_num):
+        for i in range(1,self.class_num):
             input_i = input[:,i,:,:]
             target_i = (target == i).float()
             intersect = (input_i*target_i).sum()
@@ -22,21 +22,21 @@ class DiceLoss(nn.Module):
             else:
                 dice = (2 * intersect + self.smooth) / (union + self.smooth)
             Dice += dice
-        dice_loss = 1 - Dice/(class_num - 1)
+        dice_loss = 1 - Dice/(self.class_num - 1)
         return dice_loss
 
 class EL_DiceLoss(nn.Module):
-    def __init__(self,smooth=1,gamma=0.5):
+    def __init__(self, class_num=2,smooth=1,gamma=0.5):
         super(EL_DiceLoss, self).__init__()
         self.smooth = smooth
-
+        self.class_num = class_num
         self.gamma = gamma
 
-    def forward(self,input, target, class_num):
+    def forward(self,input, target):
         input = torch.exp(input)
         self.smooth = 0.
         Dice = Variable(torch.Tensor([0]).float()).cuda()
-        for i in range(1,class_num):
+        for i in range(1,self.class_num):
             input_i = input[:,i,:,:]
             target_i = (target == i).float()
             intersect = (input_i*target_i).sum()
@@ -46,5 +46,5 @@ class EL_DiceLoss(nn.Module):
             else:
                 dice = (2 * intersect + self.smooth) / (union + self.smooth)
             Dice += (-torch.log(dice))**self.gamma
-        dice_loss = Dice/(class_num - 1)
+        dice_loss = Dice/(self.class_num - 1)
         return dice_loss
